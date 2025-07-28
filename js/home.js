5eebaf0fa365659debf3d2ce35f6b32604b07bc4
Zishan2211 <alljubayerzishan@gmail.com>
2025-07-28 16:35:37 +0600

    add money event henlre

--- js/home.js
+++ js/home.js
@@ -1,1 +1,7 @@
-console.log('connect');
+document.getElementById('but-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const inputAddmMoney = document.getElementById('input-add-money').value;
+    const inputpin = document.getElementById('input-pin').value;
+    console.log(inputAddmMoney,inputpin);
+})
