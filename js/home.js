6c485b2541eacabcc49a6509850588c5126025a5
Zishan2211 <alljubayerzishan@gmail.com>
2025-07-28 17:11:00 +0600

    add a balance and newbalance set the avabile balance

--- js/home.js
+++ js/home.js
@@ -3,5 +3,21 @@ document.getElementById('but-add-money').addEventListener('click', function(even
 
     const inputAddmMoney = document.getElementById('input-add-money').value;
     const inputpin = document.getElementById('input-pin').value;
-    console.log(inputAddmMoney,inputpin);
+
+    if(inputpin === "0000" ){
+        const accountBlance = document.getElementById('account-blance').innerText;
+        
+        const addMoneyNumber = parseFloat(inputAddmMoney);
+        const balanceNumber = parseFloat(accountBlance);
+
+        const newBalance = balanceNumber + addMoneyNumber;
+        console.log(newBalance); 
+
+        document.getElementById('account-blance').innerText = newBalance;
+        console.log(newBalance ,'set the new blance');
+    }
+    else{
+        alert('wrong pin. please try again');
+    }
+
 })
